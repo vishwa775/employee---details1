8c5335dcea2e4cd227576a41e237195b2b2d5200
vishwa775 <vishwaamagi@gmail.com>
2025-11-20 15:18:01 +0530

    save

---
+++ empdetails.py
@@ -0,0 +1,10 @@
+import sys
+emp_name = sys.argv[1]
+emp_id = sys.argv[2]
+emp_salary = sys.argv[3]
+emp_experience = sys.argv[4]
+print("Employee Details:")
+print("Name:", emp_name)
+print("ID:", emp_id)
+print("Salary:", emp_salary)    
+print("Experience:", emp_experience)
