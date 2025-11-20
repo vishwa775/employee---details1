784fb06a869294726cbb6c7ad8355e322c49b6d9
vishwa775 <vishwaamagi@gmail.com>
2025-11-20 15:20:48 +0530

    save

--- empdetails.py
+++ empdetails.py
@@ -1,10 +1,16 @@
 import sys
-emp_name = sys.argv[1]
-emp_id = sys.argv[2]
+
+if len(sys.argv) != 5:
+    print("Usage: python employee.py <emp_id> <emp_name> <emp_salary> <emp_exp>")
+    sys.exit()
+
+emp_id = sys.argv[1]
+emp_name = sys.argv[2]
 emp_salary = sys.argv[3]
-emp_experience = sys.argv[4]
-print("Employee Details:")
-print("Name:", emp_name)
+emp_exp = sys.argv[4]
+
+print("\n--- Employee Details ---")
 print("ID:", emp_id)
-print("Salary:", emp_salary)    
-print("Experience:", emp_experience)
+print("Name:", emp_name)
+print("Salary:", emp_salary)
+print("Experience:", emp_exp, "years")
